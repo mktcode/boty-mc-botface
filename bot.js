45758d1ce76a10e15d217fdbdaf4863f57106a04
mktcode <kontakt@markus-kottlaender.de>
2019-06-25 21:31:36 +0200

    adjustments for commit-tokens
    since utopian doesnt want it :P

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
-const helper = require('./helper');
+const helper = require("./helper");
 
-const DEBUG = !(process.env.BOT_DEBUG && process.env.BOT_DEBUG === 'no');
+const DEBUG = !(process.env.BOT_DEBUG && process.env.BOT_DEBUG === "no");
 
-console.log('');
+console.log("");
 console.log(new Date());
 
 if (DEBUG) {
-  console.log('############################################');
-  console.log('### DEBUG Mode: No votes will be casted. ###');
-  console.log('############################################');
+  console.log("############################################");
+  console.log("### DEBUG Mode: No votes will be casted. ###");
+  console.log("############################################");
 }
 
 const botKey = process.env.BOT_KEY; // TODO: use SteemConnect instead of SteemJS
-const botAccountName = process.env.BOT_ACCOUNT_NAME || 'mkt';
+const botAccountName = process.env.BOT_ACCOUNT_NAME || "mkt";
 
 // minimum number of posts waiting for a vote - don't vote if we are below
 const waitListSize = parseInt(process.env.BOT_WAIT_LIST_SIZE) || 100;
@@ -22,7 +22,8 @@ const minimumVotingPower = parseFloat(process.env.BOT_MIN_VOTING_POWER) || 99;
 
 // force vote if we go above - the smaller the bot interval, the closer this can be set to 100%
 // 99.93 = bot needs to run every 5 minutes, to make sure we don't reach 100%
-const maximumVotingPower = parseFloat(process.env.BOT_MAX_VOTING_POWER) || 99.93;
+const maximumVotingPower =
+  parseFloat(process.env.BOT_MAX_VOTING_POWER) || 99.93;
 
 // force vote if next post gets older than that
 // should never really happen, but could also be set to a much lower value if possible
@@ -30,122 +31,193 @@ const maxPostAgeForVotes = parseInt(process.env.BOT_MAX_POST_AGE) || 6 * 24; // 
 
 // the adjustment amplifier can adjust how "strong" voting weights will be adjusted automatically,
 // in relation to the deviation between the desired and the actual wait list.
-const adjustmentAmplifier = parseFloat(process.env.BOT_ADJUSTMENT_AMPLIFIER) || 1;
+const adjustmentAmplifier =
+  parseFloat(process.env.BOT_ADJUSTMENT_AMPLIFIER) || 1;
 
 // if the deviation is above >= 100 %, the voting weight will be the global min or max.
 // here you can limit the maximum adjustment, to deal with large deviations. (only a value < 1 makes sense)
 const maximumAdjustment = parseFloat(process.env.BOT_MAX_ADJUSTMENT) || 0.5; // adjust weights by a maximum af 50 %
 
 // global weight boundaries, applied after all adjustments
-const globalMinimumVoteWeight = parseFloat(process.env.BOT_GLOBAL_MIN_VOTE_WEIGHT) || 1;
-const globalMaximumVoteWeight = parseFloat(process.env.BOT_GLOBAL_MAX_VOTE_WEIGHT) || 50;
+const globalMinimumVoteWeight =
+  parseFloat(process.env.BOT_GLOBAL_MIN_VOTE_WEIGHT) || 1;
+const globalMaximumVoteWeight =
+  parseFloat(process.env.BOT_GLOBAL_MAX_VOTE_WEIGHT) || 100;
 
 // TODO: add more verbose logs
 
 // pull some data
-console.log('Fetching data...');
+console.log("Fetching data...");
 Promise.all([
   helper.getCurrentVotingPower(botAccountName, true),
-  helper.getPostsWaitingForUpvote(botAccountName),
-]).then(values => {
-  const currentVotingPower = values[0];
-  const postsWaitingForUpvote = values[1];
-  const nextPostToUpvote = postsWaitingForUpvote[0];
-  const nextPostMeta = JSON.parse(nextPostToUpvote.json_metadata);
+  helper.getPostsWaitingForUpvote(botAccountName)
+])
+  .then(values => {
+    const currentVotingPower = values[0];
+    const postsWaitingForUpvote = values[1];
+    if (postsWaitingForUpvote.length) {
+      const nextPostToUpvote = postsWaitingForUpvote[0];
+      const nextPostMeta = JSON.parse(nextPostToUpvote.json_metadata);
 
-  let castVote = false;
+      let castVote = false;
 
-  console.log('Checking voting conditions...');
-  console.log('Posts waiting for an upvote: ' + postsWaitingForUpvote.length + ' (desired value: ' + waitListSize + ')');
-  // check voting power first
-  if (currentVotingPower >= minimumVotingPower) {
-    // then check if there are enough posts waiting, to make some calculations based on them
-    if (postsWaitingForUpvote.length >= waitListSize) {
-      castVote = true;
-    } else {
-      console.log('Not enough posts waiting for an upvote.');
-
-      // force vote if next post gets too old
-      // TODO: maybe this should even overrule minimum voting power... ?
-      if (new Date(nextPostToUpvote.created).getTime() < (new Date()).getTime() - (maxPostAgeForVotes * 60 * 60 * 1000)) {
-        console.log('Force vote because post gets to old. (' + nextPostToUpvote.created + ')');
-        castVote = true;
-      } else {
-        // force vote if voting power gets to high
-        if (currentVotingPower >= maximumVotingPower) {
-          console.log('Force vote because Voting Power gets to high. (' + currentVotingPower.toFixed(2) + '/' + maximumVotingPower + ')');
+      console.log("Checking voting conditions...");
+      console.log(
+        "Posts waiting for an upvote: " +
+          postsWaitingForUpvote.length +
+          " (desired value: " +
+          waitListSize +
+          ")"
+      );
+      // check voting power first
+      if (currentVotingPower >= minimumVotingPower) {
+        // then check if there are enough posts waiting, to make some calculations based on them
+        if (postsWaitingForUpvote.length >= waitListSize) {
           castVote = true;
         } else {
-          console.log('No reason to force.');
+          console.log("Not enough posts waiting for an upvote.");
+
+          // force vote if next post gets too old
+          // TODO: maybe this should even overrule minimum voting power... ?
+          if (
+            new Date(nextPostToUpvote.created).getTime() <
+            new Date().getTime() - maxPostAgeForVotes * 60 * 60 * 1000
+          ) {
+            console.log(
+              "Force vote because post gets to old. (" +
+                nextPostToUpvote.created +
+                ")"
+            );
+            castVote = true;
+          } else {
+            // force vote if voting power gets to high
+            if (currentVotingPower >= maximumVotingPower) {
+              console.log(
+                "Force vote because Voting Power gets to high. (" +
+                  currentVotingPower.toFixed(2) +
+                  "/" +
+                  maximumVotingPower +
+                  ")"
+              );
+              castVote = true;
+            } else {
+              console.log("No reason to force.");
+            }
+          }
         }
+      } else {
+        console.log(
+          "Voting Power to low: " +
+            currentVotingPower.toFixed(2) +
+            "%, Waiting to reach " +
+            minimumVotingPower +
+            "% (" +
+            helper
+              .calcRecoveryTime(minimumVotingPower - currentVotingPower)
+              .toFixed(2) +
+            " minutes)"
+        );
       }
-    }
-  } else {
-    console.log('Voting Power to low: ' + currentVotingPower.toFixed(2) + '%, Waiting to reach ' + minimumVotingPower + '% (' + helper.calcRecoveryTime(minimumVotingPower - currentVotingPower).toFixed(2) + ' minutes)');
-  }
 
-  if (castVote) {
-    // adjust votes based on the deviation of desired and actual wait list size:
-    // 1. calculate deviation
-    // 2. apply amplifier
-    // 3. apply adjustment cap
-    // 4. get score based weight
-    // 5. apply final adjustment
-    // 6. apply global min/max values
+      if (castVote) {
+        // adjust votes based on the deviation of desired and actual wait list size:
+        // 1. calculate deviation
+        // 2. apply amplifier
+        // 3. apply adjustment cap
+        // 4. get score based weight
+        // 5. apply final adjustment
+        // 6. apply global min/max values
 
-    // calculate deviation percentage (e.g. 0.3 => 30 % deviation)
-    let waitListSizeDeviation = (Math.abs(waitListSize - postsWaitingForUpvote.length) / waitListSize);
-    console.log('Deviation: ' + waitListSizeDeviation);
+        // calculate deviation percentage (e.g. 0.3 => 30 % deviation)
+        let waitListSizeDeviation =
+          Math.abs(waitListSize - postsWaitingForUpvote.length) / waitListSize;
+        console.log("Deviation: " + waitListSizeDeviation);
 
-    // amplify adjustment
-    let amplifiedAdjustment = (waitListSizeDeviation * adjustmentAmplifier);
-    console.log('Amplified Adjustment: ' + amplifiedAdjustment);
+        // amplify adjustment
+        let amplifiedAdjustment = waitListSizeDeviation * adjustmentAmplifier;
+        console.log("Amplified Adjustment: " + amplifiedAdjustment);
 
-    // apply adjustment cap too avoid too drastic adjustments
-    amplifiedAdjustment = amplifiedAdjustment > maximumAdjustment ? maximumAdjustment : amplifiedAdjustment;
-    console.log('Capped Adjustment: ' + amplifiedAdjustment);
+        // apply adjustment cap too avoid too drastic adjustments
+        amplifiedAdjustment =
+          amplifiedAdjustment > maximumAdjustment
+            ? maximumAdjustment
+            : amplifiedAdjustment;
+        console.log("Capped Adjustment: " + amplifiedAdjustment);
 
-    // apply adjustment to weight multiplier
-    // if actual and desired wait list are the same, nothing will happen here
-    let votingWeightMultiplier = 1; // default: * 1 (no change)
-    if (postsWaitingForUpvote.length > waitListSize) {
-      // decrease voting power to vote faster if posts pile up
-      votingWeightMultiplier -= amplifiedAdjustment;
-    } else {
-      // increase voting power to vote slower if posts become less
-      votingWeightMultiplier += amplifiedAdjustment;
-    }
-    console.log('Final Multiplier: ' + votingWeightMultiplier.toFixed(2));
-
-    // get vote weight based on category's min and max, review score
-    let voteWeight = helper.getVoteWeightForPost(nextPostToUpvote);
-    // adjust it with the calculated multiplier
-    let adjustedVoteWeight = voteWeight * votingWeightMultiplier;
-    // apply global min/max weight values
-    if (adjustedVoteWeight < globalMinimumVoteWeight) adjustedVoteWeight = globalMinimumVoteWeight;
-    if (adjustedVoteWeight > globalMaximumVoteWeight) adjustedVoteWeight = globalMaximumVoteWeight;
-
-    console.log('Post: https://utopian.io/utopian-io/@' + nextPostToUpvote.author + '/' + nextPostToUpvote.permlink);
-    console.log('Created: ' + nextPostToUpvote.created);
-    console.log('Category: ' + nextPostMeta.type);
-    console.log('Score: ' + nextPostMeta.score + ' %');
-    console.log('Score-based Voting Weight: ' + voteWeight.toFixed(2) + ' %');
-    console.log('Adjusted Voting Weight: ' + adjustedVoteWeight.toFixed(2) + ' %');
-    console.log('Voting at ' + currentVotingPower.toFixed(2) + ' % Voting Power');
-
-    helper.upvotePost(botAccountName, botKey, nextPostToUpvote, parseInt(adjustedVoteWeight * 100)).then(() => {
-      helper.getCurrentVotingPower(botAccountName, true).then(newVotingPower => {
-        console.log('Done! New Voting Power: ' + newVotingPower.toFixed(2) + ' %');
-        if (newVotingPower < minimumVotingPower) {
-          console.log(helper.calcRecoveryTime(minimumVotingPower - newVotingPower).toFixed(2) + ' minutes to get back to ' + minimumVotingPower + ' %');
+        // apply adjustment to weight multiplier
+        // if actual and desired wait list are the same, nothing will happen here
+        let votingWeightMultiplier = 1; // default: * 1 (no change)
+        if (postsWaitingForUpvote.length > waitListSize) {
+          // decrease voting power to vote faster if posts pile up
+          votingWeightMultiplier -= amplifiedAdjustment;
+        } else {
+          // increase voting power to vote slower if posts become less
+          votingWeightMultiplier += amplifiedAdjustment;
         }
-        if (!DEBUG) {
-          // TODO: post comment
-        }
+        console.log("Final Multiplier: " + votingWeightMultiplier.toFixed(2));
+
+        // get vote weight based on category's min and max, review score
+        let voteWeight = helper.getVoteWeightForPost(nextPostToUpvote);
+        // adjust it with the calculated multiplier
+        let adjustedVoteWeight = voteWeight * votingWeightMultiplier;
+        // apply global min/max weight values
+        if (adjustedVoteWeight < globalMinimumVoteWeight)
+          adjustedVoteWeight = globalMinimumVoteWeight;
+        if (adjustedVoteWeight > globalMaximumVoteWeight)
+          adjustedVoteWeight = globalMaximumVoteWeight;
+
+        console.log(
+          "Post: @" + nextPostToUpvote.author + "/" + nextPostToUpvote.permlink
+        );
+        console.log("Created: " + nextPostToUpvote.created);
+        console.log("Score: " + nextPostMeta.score + " %");
+        console.log(
+          "Score-based Voting Weight: " + voteWeight.toFixed(2) + " %"
+        );
+        console.log(
+          "Adjusted Voting Weight: " + adjustedVoteWeight.toFixed(2) + " %"
+        );
+        console.log(
+          "Voting at " + currentVotingPower.toFixed(2) + " % Voting Power"
+        );
+
+        helper
+          .upvotePost(
+            botAccountName,
+            botKey,
+            nextPostToUpvote,
+            parseInt(adjustedVoteWeight * 100)
+          )
+          .then(() => {
+            helper
+              .getCurrentVotingPower(botAccountName, true)
+              .then(newVotingPower => {
+                console.log(
+                  "Done! New Voting Power: " + newVotingPower.toFixed(2) + " %"
+                );
+                if (newVotingPower < minimumVotingPower) {
+                  console.log(
+                    helper
+                      .calcRecoveryTime(minimumVotingPower - newVotingPower)
+                      .toFixed(2) +
+                      " minutes to get back to " +
+                      minimumVotingPower +
+                      " %"
+                  );
+                }
+                if (!DEBUG) {
+                  // TODO: post comment
+                }
+                process.exit();
+              });
+          })
+          .catch(err => console.log(err));
+      } else {
         process.exit();
-      });
-    }).catch(err => console.log(err));
-  } else {
-    process.exit();
-  }
-}).catch(err => console.log(err));
+      }
+    } else {
+      console.log("No posts available.");
+      process.exit();
+    }
+  })
+  .catch(err => console.log(err));
--- helper.js
+++ helper.js
@@ -1,24 +1,24 @@
-const steem = require('steem');
-const categories = require('./categories');
+const steem = require("steem");
+const fs = require("fs");
 
-const DEBUG = !(process.env.BOT_DEBUG && process.env.BOT_DEBUG === 'no');
+const DEBUG = !(process.env.BOT_DEBUG && process.env.BOT_DEBUG === "no");
+const DATABASE = process.env.DATABASE;
 
 const helper = {
+  getDatabase() {
+    return JSON.parse(
+      fs.readFileSync(process.env.DATABASE, { encoding: "utf-8" })
+    );
+  },
   getVoteWeightForPost(post) {
     let meta = JSON.parse(post.json_metadata);
-    let category = meta.type;
-    category = category.indexOf('task-') !== -1 ? 'task-requests' : category;
-
-    let min = categories.limits[category].min;
-    let max = categories.limits[category].max;
-
-    return ((max - min) * (meta.score / 100)) + min;
+    return meta.score;
   },
   calcRecoveryTime(percentage) {
     return percentage * 72;
   },
   calcDecreaseVP(voteWeight) {
-    return 2 * voteWeight / 100;
+    return (2 * voteWeight) / 100;
   },
   getCurrentVotingPower(account, humanReadable = false) {
     return new Promise((resolve, reject) => {
@@ -26,8 +26,11 @@ const helper = {
         if (!err) {
           const status = accounts[0];
 
-          const secondsAgo = (new Date().getTime() - new Date(status.last_vote_time + 'Z').getTime()) / 1000;
-          let votingPower = status.voting_power + (10000 * secondsAgo / 432000);
+          const secondsAgo =
+            (new Date().getTime() -
+              new Date(status.last_vote_time + "Z").getTime()) /
+            1000;
+          let votingPower = status.voting_power + (10000 * secondsAgo) / 432000;
           votingPower = votingPower > 10000 ? 10000 : votingPower;
 
           if (humanReadable) votingPower /= 100;
@@ -36,57 +39,43 @@ const helper = {
         } else {
           reject(err);
         }
-      })
+      });
     });
   },
   async getPostsWaitingForUpvote(voterAccount) {
-    let postsWaitingForUpvote = [];
-    let maxAge = 6.5 * 24 * 60 * 60 * 1000;
-    let posts;
-    let lastPost;
-    let lastPostAge;
-    let startAuthor;
-    let startPermlink;
+    const postsWaitingForUpvote = [];
+    const maxAge = 6.4 * 24 * 60 * 60 * 1000;
+    const posts = await this.getPosts();
+    const database = this.getDatabase();
 
-    do {
-      posts = await this.getPosts(startAuthor, startPermlink);
-      lastPost = posts[posts.length - 1];
-      lastPostAge = new Date().getTime() - new Date(lastPost.created).getTime();
-      startAuthor = lastPost.author;
-      startPermlink = lastPost.permlink;
+    for (let i = 0; i < posts.length; i++) {
+      let post = posts[i];
+      let postAge = new Date().getTime() - new Date(post.created).getTime();
+      let meta = JSON.parse(post.json_metadata);
+      let hasScore = meta.hasOwnProperty("score") && meta.score > 0 === true;
+      let isVoted = this.isVoted(post.active_votes, voterAccount);
+      let isInDatabase = database.find(pr => pr.id === meta.prId);
 
-      for (let i = 0; i < posts.length; i++) {
-        let post = posts[i];
-        let postAge = new Date().getTime() - new Date(post.created).getTime();
-        let meta = JSON.parse(post.json_metadata);
-        let isUtopianPost = meta.hasOwnProperty('app') && meta.app.indexOf('utopian') !== -1;
-        let isApproved = meta.hasOwnProperty('moderator') && meta.moderator.reviewed === true;
-        let hasScore = meta.hasOwnProperty('score') && meta.score > 0 === true;
-        let isVoted = this.isVoted(post.active_votes, voterAccount);
-
-        if (postAge < maxAge && isUtopianPost && isApproved && hasScore && !isVoted) {
-          postsWaitingForUpvote.push(post);
-        }
+      if (isInDatabase && postAge < maxAge && hasScore && !isVoted) {
+        postsWaitingForUpvote.push(post);
       }
-
-    } while (lastPostAge < maxAge);
+    }
 
     return postsWaitingForUpvote.reverse(); // reverse to have oldest post first
   },
-  getPosts(start_author, start_permlink) {
+  getPosts() {
     return new Promise((resolve, reject) => {
-      steem.api.getDiscussionsByCreated({
-        tag: 'utopian-io',
-        limit: 100,
-        start_author: start_author,
-        start_permlink: start_permlink
-      }, (err, res) => {
-        if (!err) {
-          resolve(res);
-        } else {
-          reject(err);
+      steem.api.getContentReplies(
+        "mkt",
+        "mobile-optimization-hotfix-for-steem-engine-com",
+        (err, res) => {
+          if (!err) {
+            resolve(res);
+          } else {
+            reject(err);
+          }
         }
-      });
+      );
     });
   },
   isVoted(activeVotes, voterAccount) {
@@ -98,40 +87,42 @@ const helper = {
     }
     return voted;
   },
-  getStatsFromPast(startDate, endDate) {
-    return new Promise((resolve, reject) => {
-      const statsFromPast = {
-        votedPostsTotal: 500,
-        votedPostsPerDay: 71.4,
-        averageVotingWeight: 1300, // actual used voting weight
-        averageScore: 75,
-      };
-      resolve(statsFromPast);
-    });
-  },
   upvotePost(voter, key, post, weight) {
     return new Promise((resolve, reject) => {
-      steem.api.getActiveVotes(post.author, post.permlink, (err, activeVotes) => {
-        if (!err) {
-          if (!this.isVoted(activeVotes, voter)) {
-            if (!DEBUG) {
-              steem.broadcast.vote(key, voter, post.author, post.permlink, weight, (err, result) => {
-                if (!err) {
-                  resolve(result);
-                } else {
-                  reject(err);
-                }
-              });
+      steem.api.getActiveVotes(
+        post.author,
+        post.permlink,
+        (err, activeVotes) => {
+          if (!err) {
+            if (!this.isVoted(activeVotes, voter)) {
+              if (!DEBUG) {
+                steem.broadcast.vote(
+                  key,
+                  voter,
+                  post.author,
+                  post.permlink,
+                  weight,
+                  (err, result) => {
+                    if (!err) {
+                      resolve(result);
+                    } else {
+                      reject(err);
+                    }
+                  }
+                );
+              } else {
+                resolve();
+              }
             } else {
-              resolve();
+              console.log(
+                "Already voted on " + post.author + "/" + post.permlink
+              );
             }
           } else {
-            console.log('Already voted on ' + post.author + '/' + post.permlink);
+            reject(err);
           }
-        } else {
-          reject(err);
         }
-      });
+      );
     });
   }
 };
