3a6c8a974a4587ce4fdab08ab6ed43b042fa069f
mktcode <kontakt@markus-kottlaender.de>
2019-07-29 20:32:24 +0200

    cron script for fetching transactions

--- bounties.js
+++ bounties.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const steem = require("steem");
 const mysql = require("mysql");
 require("dotenv").config({ path: __dirname + "/.env" });
 
@@ -9,58 +10,157 @@ const database = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-const QUERY_BOUNTIES =
-  "SELECT * FROM bounties WHERE claimedAt IS NULL ORDER BY createdAt ASC";
-const UPDATE_BOUNTY = "UPDATE bounties SET balance = ? WHERE id = ?";
+const QUERY_OLDEST_OPEN_BOUNTY_DATE =
+  "SELECT MIN(createdAt) as oldestDate FROM bounties WHERE releasedAt IS NULL ORDER BY createdAt DESC";
 
-database.query(QUERY_BOUNTIES, (error, bounties) => {
+const QUERY_OPEN_BOUNTIES =
+  "SELECT * FROM bounties WHERE releasedAt IS NULL AND createdAt >= ?";
+
+const QUERY_BOUNTY_DEPOSIT_EXISTS =
+  "SELECT id FROM bountyDeposits WHERE txId = ?";
+
+const INSERT_BOUNTY_DEPOSIT =
+  "INSERT INTO bountyDeposits (receivingAddress, sbdAmount, currency, bountyId, txId) VALUES (?, ?, ?, ?, ?)";
+
+const getBlocktradesTransactions = dateLimit => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post("https://blocktrades.us/api/v2/sessions", {
+        email: process.env.BLOCKTRADES_EMAIL,
+        password: process.env.BLOCKTRADES_PASS
+      })
+      .then(response => {
+        const session = response.data;
+        axios
+          .get("https://blocktrades.us/api/v2/transactions", {
+            params: {
+              sessionToken: session.token,
+              lastModifiedSince: dateLimit
+            }
+          })
+          .then(response => {
+            resolve(response.data);
+          })
+          .catch(e => reject(e));
+      })
+      .catch(e => reject(e));
+  });
+};
+
+const getSbdTransactions = dateLimit => {
+  return new Promise((resolve, reject) => {
+    steem.api.getAccountHistory("merge-rewards", -1, 10000, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        transactions = [];
+        result.forEach(r => {
+          if (r[1].op[0] === "transfer" && r[1].op[1].to === "merge-rewards") {
+            transactions.push(r);
+          }
+        });
+        resolve(transactions);
+      }
+    });
+  });
+};
+
+database.query(QUERY_OLDEST_OPEN_BOUNTY_DATE, (error, result) => {
   if (error) {
     console.log(error);
   } else {
-    bounties.forEach(bounty => {
-      const amountRequests = [];
-      [
-        "btcAddress",
-        "ltcAddress",
-        "ethAddress",
-        "xmrAddress",
-        "steemAddress"
-      ].forEach(type => {
-        amountRequests.push(
-          axios
-            .get("https://blocktrades.us/api/v2/transactions/", {
-              params: {
-                inputAddress: bounty[type],
-                sessionToken: bounty.sessionToken,
-                lastModifiedSince: bounty.createdAt
-              }
-            })
-            .then(response => {
-              const transactions = response.data.filter(
-                tx => tx.inputAddress === bounty[type]
-              );
-              let totalAmount = 0;
-              transactions.forEach(tx => {
-                totalAmount += Number(tx.outputAmount);
+    const oldestDate = result[0].oldestDate;
+    database.query(QUERY_OPEN_BOUNTIES, [oldestDate], (error, openBounties) => {
+      if (error) {
+        console.log(error);
+      } else {
+        if (openBounties.length) {
+          Promise.all([
+            getBlocktradesTransactions(oldestDate),
+            getSbdTransactions(oldestDate)
+          ]).then(results => {
+            const blocktradesTransactions = results[0];
+            const sbdTransactions = results[1];
+            blocktradesTransactions.forEach(bt => {
+              const receivingBounty = openBounties.find(ob => {
+                return (
+                  (bt.inputCoinType === "btc" &&
+                    bt.inputAddress === ob.btcAddress) ||
+                  (bt.inputCoinType === "ltc" &&
+                    bt.inputAddress === ob.ltcAddress) ||
+                  (bt.inputCoinType === "eth" &&
+                    bt.inputAddress === ob.ethAddress) ||
+                  (bt.inputCoinType === "xmr" &&
+                    bt.inputAddress === ob.xmrAddress) ||
+                  (bt.inputCoinType === "steem" &&
+                    bt.inputAddress === ob.steemAddress)
+                );
               });
-              return totalAmount;
-            })
-            .catch(e => console.log(e.response.data.error))
-        );
-      });
 
-      Promise.all(amountRequests).then(amounts => {
-        const totalAmount = amounts.reduce((total, amount) => {
-          return total + amount;
-        }, 0);
-        database.query(UPDATE_BOUNTY, [totalAmount, bounty.id], error => {
-          if (error) {
-            console.log(error);
-          } else {
-            process.exit();
-          }
-        });
-      });
+              if (receivingBounty) {
+                database.query(
+                  QUERY_BOUNTY_DEPOSIT_EXISTS,
+                  [bt.transactionId],
+                  (error, result) => {
+                    if (error) {
+                      console.log(error);
+                    } else if (!result.length) {
+                      database.query(
+                        INSERT_BOUNTY_DEPOSIT,
+                        [
+                          bt.inputAddress,
+                          bt.outputAmount,
+                          bt.inputCoinType,
+                          receivingBounty.id,
+                          bt.transactionId
+                        ],
+                        error => {
+                          if (error) {
+                            console.log(error);
+                          }
+                        }
+                      );
+                    }
+                  }
+                );
+              }
+            });
+            sbdTransactions.forEach(t => {
+              const receivingBounty = openBounties.find(ob => {
+                return ob.sbdAddress === t[1].op[1].memo;
+              });
+
+              if (receivingBounty) {
+                database.query(
+                  QUERY_BOUNTY_DEPOSIT_EXISTS,
+                  [t[1].trx_id],
+                  (error, result) => {
+                    if (error) {
+                      console.log(error);
+                    } else if (!result.length) {
+                      database.query(
+                        INSERT_BOUNTY_DEPOSIT,
+                        [
+                          t[1].op[1].memo,
+                          parseFloat(t[1].op[1].amount.replace(" SBD", "")),
+                          "sbd",
+                          receivingBounty.id,
+                          t[1].trx_id
+                        ],
+                        error => {
+                          if (error) {
+                            console.log(error);
+                          }
+                        }
+                      );
+                    }
+                  }
+                );
+              }
+            });
+          });
+        }
+      }
     });
   }
 });
